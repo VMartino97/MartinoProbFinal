02364350c3df7381219dee6a0f6c0b50bdb59b62
VMartino97 <44848782+VMartino97@users.noreply.github.com>
2018-12-04 11:19:52 -0600

    simple rejection sampling

--- R/pdf function.R
+++ R/pdf function.R
@@ -1,3 +1,9 @@
-###Function Page
-install.packages("roxygen2")
-function(n, pdf, a, b, c)
+###Basic Rejection Sampling
+
+accept.reject <- function(n, pdf, a, b, c){
+  Q <- replicate (n, {X <- runif(a,b)
+  Y <- runif(0,c)
+  if (pdf(x)>=Y){accept <- TRUE}
+  else {accept <- FALSE}})
+  Q
+}
