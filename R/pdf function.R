5c41957a9b91b4facebff87a7e2e457d77dcb8b8
VMartino97 <44848782+VMartino97@users.noreply.github.com>
2018-12-05 13:27:53 -0600

    go roxygen2 going for the namespace and working on vignnette

--- R/pdf function.R
+++ R/pdf function.R
@@ -1,7 +1,7 @@
 ###Basic Rejection Sampling
-accept.reject <- function(n, pdf, a, b, c){
+accept.reject <- function(n, pdf, a, b, C){
   Q <- replicate (n, {u <- runif(a,b)
-  Y <- runif(0,c)
+  Y <- runif(0,C)
   if (pdf(u)>=Y){accept <- TRUE}
   else {accept <- FALSE}})
   Q
