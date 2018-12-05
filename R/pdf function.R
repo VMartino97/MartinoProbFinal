c6b6841cc2f657d9a82c0f5357e054e18cf20f6b
VMartino97 <44848782+VMartino97@users.noreply.github.com>
2018-12-05 13:38:07 -0600

    might have gotten my function to work, not sure?
    I do not know what the .553... file is

--- R/pdf function.R
+++ R/pdf function.R
@@ -1,8 +1,8 @@
 ###Basic Rejection Sampling
 accept.reject <- function(n, pdf, a, b, C){
-  Q <- replicate (n, {u <- runif(a,b)
-  Y <- runif(0,C)
-  if (pdf(u)>=Y){accept <- TRUE}
+  Q <- replicate (n, {u <- runif(1,a,b)
+  Y <- runif(1,0,C)
+  if (pdf >= Y){accept <- TRUE}
   else {accept <- FALSE}})
   Q
 }
