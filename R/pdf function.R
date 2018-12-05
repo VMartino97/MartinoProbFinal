42772f276ace8b3d04ba5daf2e24d81133b2d753
VMartino97 <44848782+VMartino97@users.noreply.github.com>
2018-12-05 14:10:42 -0600

    added result storage to algorithm, still needs work

--- R/pdf function.R
+++ R/pdf function.R
@@ -1,8 +1,14 @@
 ###Basic Rejection Sampling
 accept.reject <- function(n, pdf, a, b, C){
+  n.accepts <- 0
+  result.sample <- rep(NA, n)
+
   Q <- replicate (n, {u <- runif(1,a,b)
   Y <- runif(1,0,C)
-  if (pdf >= Y){accept <- TRUE}
-  else {accept <- FALSE}})
-  Q
+  if (pdf >= Y){
+    n.accepts <- n.accepts+1
+    result.sample[n.accepts] = C
+  }
+  })
+  result.sample
 }
