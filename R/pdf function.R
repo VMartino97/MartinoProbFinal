d911ab245978dc97024220445d6b56161f5fcedf
VMartino97 <44848782+VMartino97@users.noreply.github.com>
2018-12-06 11:11:51 -0600

    updated vignette and slight code fixes

--- R/pdf function.R
+++ R/pdf function.R
@@ -4,11 +4,12 @@ accept.reject <- function(n, pdf, a, b, C){
   result.sample <- rep(NA, n)
 
   while(n.accepts < 0){
-  Q <- runif(1,a,b)
-  Y <- runif(1,0,C)
-  if (pdf(Q) >= Y){
+  q<- runif(1,a,b)
+  y <- runif(1,0,C)
+  if (pdf(q)>=y){
     n.accepts <- n.accepts+1
-    result.sample[n.accepts] = Q
+    result.sample[n.accepts] = q
   }}
+
   result.sample
 }
