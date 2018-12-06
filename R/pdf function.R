904a64c2f2dc05fdbd58cd9462ddef93060381e3
VMartino97 <44848782+VMartino97@users.noreply.github.com>
2018-12-06 11:48:25 -0600

    cleaning things up

--- R/pdf function.R
+++ R/pdf function.R
@@ -1,4 +1,15 @@
-###Basic Rejection Sampling
+#' @title Martino Prob Final
+#'
+#' @param n is the desired number of samples
+#' @param pdf is the pdf of the random variable that you wish to sample from
+#' @param a is the lower bound of the random variable
+#' @param b is the upper bound of the random variable
+#' @param C is the number that f(x) <= C for all x
+#'
+#' @description samples a give pdf of a random variable
+#'
+#' @return the samples
+
 accept.reject <- function(n, pdf, a, b, C){
   n.accepts <- 0
   result.sample <- rep(NA, n)
